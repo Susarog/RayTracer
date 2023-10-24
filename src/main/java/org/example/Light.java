cf5f608f11bbe411368a8223ad88196c94297345
Ryan Hang <ryanhang92802@gmail.com>
2023-10-24 16:53:24 -0700

    TODO: FIX phong lighting

--- src/main/java/org/example/Light.java
+++ src/main/java/org/example/Light.java
@@ -10,6 +10,27 @@ public class Light {
         this.intensity = intensity;
     }
 
+    public Color lighting(Material material, Point position, Vector eyeVec, Vector normalVec) {
+        // combine the surface color with the light's color
+        Color effectiveColor = material.getColor().hadamardProduct(intensity);
+        Vector lightVec = this.position.subtract(position);
+        Color ambient = effectiveColor.multiply(material.getAmbient());
+        float diffuse = 0;
+        float specular = 0;
+        float lightDotNormal = lightVec.dotProduct(normalVec);
+        if (lightDotNormal >= 0 ) {
+            diffuse = effectiveColor.multiply(material.getDiffuse()).multiply(lightDotNormal);
+            Vector reflectVec = lightVec.negate().reflect(normalVec);
+            float reflectDotEye = reflectVec.dotProduct(eyeVec);
+
+            if (reflectDotEye > 0) {
+                float factor = Math.pow(reflectDotEye, material.getShininess());
+                specular = 0;
+            }
+        }
+        float finalShading = ambient + diffuse + specular;
+        return new Color(finalShading, finalShading, finalShading) ;
+    }
     public Point getPosition() {
         return position;
     }
